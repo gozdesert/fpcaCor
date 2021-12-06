d543c4e2b1510a4b9d2b8ccb0e8cb44f3a349909
Gozde Sert <gozdesert@tamu.edu>
2021-12-06 12:04:20 -0600

    Added gaussian_copula_cor function to calculate pointwise correlation matrix

---
+++ R/gaussian_copula_cor.R
@@ -0,0 +1,18 @@
+#To get pointwise correlation from Gaussian latent model
+# We construct a sample correlation matrix using latentcor func
+#' Pointwise Correlation from Gaussian latent model
+#'
+#' @param n
+#' @param ntime
+#' @return pointwise correlation matrix
+#' @export
+#'
+#' @examples
+#' gaussian_copula_cor(n = 100, ntime = 20)
+#' gaussian_copula_cor(n = 20, ntime = 2)
+
+gaussian_copula_cor = function(n = 100, ntime = 50){
+  T_types = rep("con", ntime)
+  sim_data = gen_data(n, types = T_types)$X
+  cor_pointwise = latentcor(X = sim_data)$Rpointwise #return this pointwise correlation matrix
+}
