23e85ec458ae1de910e3c755b2effd4386e2958f
Gozde Sert <gozdesert@tamu.edu>
2021-12-07 09:24:56 -0600

    Debugged the code by adding paranthesis.

--- R/gaussian_copula_cor.R
+++ R/gaussian_copula_cor.R
@@ -6,12 +6,14 @@
 #' @param n A positive integer indicating the sample size. The default value is 100.
 #' @param ntime A positive integer indicating the number of time points. The default value is 50.
 #' @return A list with the elements
-#'   \item{Y}{A n x ntime matrix for the Gaussian latent model
+#'   \item{Y}{A n x ntime matrix for the Gaussian latent model}
 #'   \item{Khat}{A ntime x ntime pointwise correlation matrix for the Gaussian latent model}
 #' @export
 #'
 #' @examples
+#'
 #' gaussian_copula_cor(n = 100, ntime = 20)
+#'
 #' gaussian_copula_cor(n = 20, ntime = 2)
 
 gaussian_copula_cor = function(n = 100, ntime = 50){
